d9257104f4bf9057d1ddc03a899606393f3c1a87
srinuammu2326 <a.srinu2605@gmail.com>
2024-11-12 12:05:14 +0530

    commit

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
 // import Child from './Child'
 
 export default function App() {
@@ -7,7 +8,35 @@ export default function App() {
   // function demo(udi){
   //   alert("Welcome"+udi)
   // }
-  const[state, setState] = useState("single app application")
+  // const[data, setData] = useState(0)
+  // const add=()=>{
+  //   setData(data+1)
+  // }
+  // const reset=()=>{
+  //   setData(0)
+  // }
+  // const del=()=>{
+  //   setData(data-1)
+  // }
+  // const person={
+  //   fname:'Rohit',
+  //   lname:'Sharama'
+
+  // }
+  // const person1={
+  //   fname:'Virat',
+  //   lname:'Kohil'
+  // }
+
+// const[user, setUser]=useState(person)
+// const handler2=()=>{
+//   setUser(person1)
+// }
+ const [data,setData] = useState({ fname:'Virat',lname:'Kohil'})
+ function demo(){
+  setData({...data, fname:"Ramesh"})
+ }
+
   return (
 //     <div>
 //  <h1>App Component</h1>
@@ -18,9 +47,26 @@ export default function App() {
 //  <button onMouseOver={demo}>MouseOver</button>
 //  <button onMouseOut={demo}>MouseOut</button>
 //     </div>
-   <>
-   <h1>{state}</h1>
-   </>
+  //  <>
+  //  <h1>{data}</h1>
+  //  <button onClick={add} className='btn btn-primary'>Increment</button>
+  //  <button onClick={reset} className='btn btn-secondary'>update State</button>
+  //  <button onClick={del} className='btn btn-success'>Drement</button>
+  //  </>
+
+
+  // <>
+  //   <h1> App Component</h1>
+  //   <h3>{user.fname} {user.lname}</h3>
+  //   <button onClick={handler2}>update Object</button>
+  // </>
+
+  <>
+  <h1> Hello</h1>
+  My first name is {data.fname} and last name is {data.lname}
+  <br/>
+  <button onClick={demo}>update</button>
+  </>
 
   )
 }
--- firstapp/src/CompA.js
+++ firstapp/src/CompA.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import CompB from './CompB'
 
-export default function CompA({data}) {
+export default function CompA({dat}) {
   return (
     <div>
         <h1>CompA</h1>
         {/* <h1>{props.x}</h1> */}
         {/* <img src = {props.x} alt='' /> */}
         {/* <h1>{props.data[1]} {props.data1.fname}</h1> */}
-        <CompB data={data}/>
+        <CompB data={dat}/>
     </div>
   )
 }
